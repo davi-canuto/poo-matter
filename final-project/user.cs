dd747bb6b7c4d5f843ee4ba7717d6db455ebbef9
Davi-Canuto <>
2021-12-30 17:50:20 +0000

    Project-final

---
+++ final-project/user.cs
@@ -0,0 +1,28 @@
+using System;
+
+class User{
+  private int id;
+  private string name;
+
+  public User(int id, string name){
+    this.id = id;
+    this.name = name;
+  }
+  public void SetId(int id){
+    this.id = id;
+  }
+
+  public void SetName(string name){
+    this.name = name;
+  }
+  public int GetId(){
+    return id;
+  }
+  public string GetName(){
+    return name;
+  }
+
+  public override string ToString(){
+    return id + " - " + name;
+  }
+}
