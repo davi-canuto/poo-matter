760e231900ac9f69e4c072ea2d90439beced54c1
davi-canuto <davicanutogregorio@gmail.com>
2022-03-15 10:24:25 -0300

    perto do fim

--- project/exemplary.cs
+++ project/exemplary.cs
@@ -1,14 +1,49 @@
 using System;
 
 class Exemplary {
-  public int id { get; set; }
-  public string title { get; set; }
-  public int cart { get; set; }
+  public int id;
+  public string title;
+  private Cart cart;
+  private Book book;
+  public Exemplary() { }
+
+  public Exemplary(int id, string title) {
+    this.id = id;
+    this.title = title;
+  }
+  public Exemplary(int id, string title, Cart cart, Book book) : this(id,title){
+    this.cart = cart;
+    this.book = book;
+  }
+  public void SetId(int id) {
+    this.id = id;
+  }
+  public void SetTitle(string title) {
+    this.title = title;
+  }
+  public void SetCart(Cart cart) {
+    this.cart = cart;
+  }
+  public void SetBook(Book book) {
+    this.book = book;
+  }
+  public int GetId() {
+    return id;
+  }
+  public string GetTitle() {
+    return title;
+  }
+  public Cart GetCart() {
+    return cart;
+  }
+  public Book GetBook() {
+    return book;
+  }
 
   public override string ToString(){
     if (cart == null)
       return id + " - " + title;
     else
-      return id + " - " + title + " -  Cart_id: " + cart;
+      return id + " - " + title + " -  Cart_id: " + cart.GetId();
   }
 }
--- project/ncart.cs
+++ project/ncart.cs
@@ -1,34 +1,60 @@
 using System;
-using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Text;
+using System.IO;
 
 class NCart {
-  private List<Cart> carts = new List<Cart>();
+  private NCart(){ }
+  static NCart obj = new NCart();
+  public static NCart Singleton { get => obj; }
 
-  public void Insert(Cart l){
-    int max = 0;
-    foreach(Cart obj in carts)
-      if (obj.id > max) max = obj.id;
-    l.id = max + 1;
-    carts.Add(l);
+  private Cart[] carts = new Cart[10];
+  private int np;
+  public void Open(){
+    Archive<Cart[]> f = new Archive<Cart[]>();
+    carts = f.Open("./carts.xml");
+    np = carts.Length;
   }
-
-  public List<Cart> List() {
-    return carts;
+  public void ToSave(){
+    Archive<Cart[]> f = new Archive<Cart[]>();
+    f.ToSave("./carts.xml", List());
   }
-
+  public Cart[] List() {
+    Cart[] p = new Cart[np];
+    Array.Copy(carts, p, np);
+    return p;
+  }
   public Cart List(int id) {
-    for (int i = 0 ; i < carts.Count; i++)
-      if (carts[i].id == id) return carts[i];
+    for (int i = 0; i < np; i++)
+      if (carts[i].GetId() == id) return carts[i];
     return null;
   }
 
-  public void Update(Cart l){
-    Cart c_atual = List(l.id);
-    if (c_atual == null) return;
-    c_atual.capacity = l.capacity;
+  public void Insert(Cart p) {
+    if (np == carts.Length) {
+      Array.Resize(ref carts, 2 * carts.Length);
+    }
+    carts[np] = p;
+    np++;
   }
 
-  public void Delete(Cart l){
-    if (l != null) carts.Remove(l);
+  public void Update(Cart p) {
+    Cart p_atual = List(p.GetId());
+    if (p_atual == null) return;
+    p_atual.SetCapacity(p.GetCapacity());
+  }
+
+  private int Indice(Cart p) {
+    for(int i = 0; i < np; i++)
+      if (carts[i] == p) return i;
+    return -1;
+  }
+
+  public void Delete(Cart p) {
+    int n = Indice(p);
+    if (n == -1) return;
+    for (int i = n; i < np - 1; i++)
+      carts[i] = carts[i + 1];
+    np--;
   }
 }
